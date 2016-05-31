074d277d596ccad37c460245af77b5678421c5df
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-05-31 16:11:49 -0500

    solved for missing methods and printed true/false based on result

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,30 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+#replace will also work for all of the below 
 
-# "zom".<???>
-# => “zoom”
+p "iNvEsTiGaTiOn".swapcase == "InVeStIgAtIoN"
 
-# "enhance".<???>
-# => "    enhance    "
+p "zom".insert(2, "o") == "zoom"
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+p "enhance".center(15) == "    enhance    "
 
-# "the usual".<???>
-#=> "the usual suspects"
+p "Stop! You’re under arrest!".upcase == "STOP! YOU’RE UNDER ARREST!"
 
-# " suspects".<???>
-# => "the usual suspects"
+p "the usual".insert(9, " suspects") == "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+p " suspects".insert(0, "the usual") == "the usual suspects"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+p "The case of the disappearing last letter".chop == "The case of the disappearing last lette"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+p ("The mystery of the missing first letter".delete "T") == "he mystery of the missing first letter"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+p "Elementary,    my   dear        Watson!".squeeze(" ") == "Elementary, my dear Watson!"
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+p "z".ord == 122 
+#122 is the corresponding integer location as defined by the ASC for "z" - where "a" is 97. Capital letters and symbols also have corresponding integer values.
+
+p ("How many times does the letter 'a' appear in this string?".count "a") == 4
+
+
+
