c052f41647c409973d583abea76034b759b820a7
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-01 15:35:16 -0500

    allowed for user corrections and printed final results

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,5 @@
 #Create a Hash to stored user data
-survey = Hash.new("oops")
+survey = Hash.new
 #Prompt user for information
 #Store information in hash as appropriate data types
 puts "What is your full name?"
@@ -14,13 +14,24 @@ survey[:favorite_color] = gets.chop
 puts "Have you graduated College? (yes/no)"
 college = gets.chomp
 if college == "yes" 
-	survey[:College_Graduate] = true
+	survey[:college_graduate] = true
 else 
-	survey[:College_Graduate] = false
+	survey[:college_graduate] = false
 end
 #Return result when all information is collected
 p survey
 #Prompt user for any corrections
-#Store data and update hash if applicable
-#Print finalized Hash
+puts "Do you need to make any corrections? (yes/no)"
+corrections = gets.chomp
+#if correctios are necessary, store new data in hash
+if corrections == "yes"
+	puts "What do you need to change?"
+	change_value = gets.chomp
+	change_value = change_value.gsub(/\s+/,"_").to_sym
+	puts "What would you like to change it to?"
+	change = gets.chomp
+	survey[change_value] = change
+end 
 
+#print updated hash
+p survey
