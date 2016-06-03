89f2b6b107a43e36ec49b02b5abcdd967acfa5ae
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-03 12:42:01 -0500

    added nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,25 @@
+bookshelf = {
+"top" => [
+	"candle", 
+	"ferbie", 
+	"flowers"
+	],
+"first row" => [
+	"partner's record collection", 
+	"old school binders", 
+	"pokemon cards"
+	],
+"second row" => [
+	"pencil holder",
+	"stapler",
+	"textbooks"
+	],
+
+"third row" => [
+	"Wheel of Time Series", 
+	"Mistborn Triology",
+	"All of Ursula K. Le Guin's Novels"
+	],
+}
+
+p bookshelf
