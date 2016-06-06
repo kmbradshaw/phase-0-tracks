51b56d90e55355cc310bfd49034baa3dd0dbdcda
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-05 20:09:01 -0500

    defined two methods, one for locating individual letters, the second to iterate through the letters and combine into an encrypted value

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,35 @@
+#create a method for locating vowels, spaces and consonants
+#if consonant, translate to next consonant
+#if vowel, translate to next vowel
+def alias_char(char)
+	i = 0 
+	vowels = "aeiou"
+	consonants = "bcdfghjklmnpqrstvwxyz"
+	if char == " "
+		" "
+#removes edge case issues
+	elsif char == "z"
+		"b"
+	elsif char == "u"
+		"a"
+	elsif consonants.index(char)
+		locate_consonant = consonants.index(char[i])
+		new_consonant = consonants[locate_consonant + 1]
+	else
+		vowels.index(char)
+		locate_vowel = vowels.index(char[i])
+		new_vowel = vowels[locate_vowel + 1]
+end 
+end
+
+def alias_name(name)
+char_index = 0 
+alias_response = ""
+	while char_index < name.length
+	alias_response << alias_char(name.downcase[char_index])
+	char_index += 1
+	end
+	alias_response
+end 
+
+alias_name("u")
