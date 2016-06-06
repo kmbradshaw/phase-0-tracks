b6ee3874b3f1c00c89639031b591eb5f2a7d3593
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-05 22:05:00 -0500

    added user interface, psuedocode for missing aspects

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -22,14 +22,31 @@ def alias_char(char)
 end 
 end
 
+#iterates through each letter, combing into translated alias
 def alias_name(name)
-char_index = 0 
-alias_response = ""
-	while char_index < name.length
-	alias_response << alias_char(name.downcase[char_index])
-	char_index += 1
-	end
+	char_index = 0 
+	alias_response = ""
+		while char_index < name.length
+			alias_response << alias_char(name.downcase[char_index])
+			char_index += 1
+		end
 	alias_response
 end 
 
-alias_name("u")
+alias_storage = []
+
+loop do 
+	puts "Please enter your full name"
+	full_name = gets.chomp
+	puts "Your spy name is #{alias_name(full_name)}"
+	alias_storage << full_name
+	alias_storage << alias_name(full_name)
+	puts "If you would like to enter another name press enter, otherwise type 'q' for quit"
+	repeat = gets.chomp
+	break if repeat == "q"
+end 
+
+p alias_storage 
+
+## Print out interpolate array
+## Switch First and Last Name
