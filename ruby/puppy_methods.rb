7ce2c82c206eecd281a855f91613f51156f313b9
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-09 17:19:25 -0500

    added methods to puppy class and test calls

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
 class Puppy
 
-  def fetch(toy)
+	def initialize
+		puts "intializing new puppy instance..."
+	end 
+	
+	def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
-  end
+  	end
+
+  def speak(number)
+  	number.times do  
+  	 puts "Woof!"
+  	end 
+end
+
+  def roll_over
+	puts "*rolls over*"
+	end 
+ def dog_years(number)
+ 	p number * 7
+	end 
+	def quiet_bark
+		puts "...woof..."
+	end 
 
 end
 
+Puppy.new.fetch("ball")
+Puppy.new.speak(3)
+Puppy.new.roll_over
+Puppy.new.dog_years(1)
+Puppy.new.quiet_bark
