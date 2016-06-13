683aa35a2eb24d1370be714278b3609d0df95d22
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-13 02:15:47 -0500

    added design pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,8 @@
+# Cat Class
+# Attributes - Age, Breed, Sex
+# A method that takes a number of pets and returns the cats action
+# A dinner time method that prints a string demanding food
+# A method that takes an item of furniture and returns a string declaring that it has been scratched
+
+
+
