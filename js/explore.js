c768817c0ec44ff52846d82d8d1b49a15413dbf9
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-06-14 09:49:39 -0500

    added pseudocode

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+// Create a functin that reverses a string
+// declare an empty variable
+// create a method that takes the variable as input
+// beginning at the end last letter OF the word, print each letter until all letters have been printed
+
+// driver code
+// store reserved word as it's own variable
+
+// print the stored variable if a condition evaluates to true
+
+
