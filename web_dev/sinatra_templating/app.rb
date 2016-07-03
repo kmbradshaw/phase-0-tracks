33fe22726e2181b9881eb80da0349adc40dbe367
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-07-03 11:22:53 -0500

    added cities tables

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,9 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+db1 = SQLite3::Database.new("cities.db")
+db1.results_as_hash = true
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -17,7 +20,11 @@ get '/students/new' do
   erb :new_student
 end
 
-get '/DBC_Cities/new' do 
+get '/cities/new' do 
+	erb :new_cities
+end 
+
+get "/DBC_cities" do 
 	erb :DBC_cities
 end 
 
@@ -25,6 +32,11 @@ end
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+post '/cities' do
+  db1.execute("INSERT INTO cities (city) VALUES (?)", [params['city']])
   redirect '/'
 end
 
